c95b612b2818e6f5cdf2cd9bb8424221a37e6f0d
Joanna Bach <joannakatarzynabach@gmail.com>
2022-01-27 15:17:06 +0100

    Add .active to links

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
-}
 
-/* remove class 'active' from all article links  */
-const activeLinks = document.querySelectorAll('.titles a.active');
+  /* [DONE] remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
+  for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
-}
+  }
 
-  /* add class 'active' to the clicked link */
+/* [IN PROGRESS] add class 'active' to the clicked link */
   
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.post');
+  clickedElement.classList.add('active');
+  console.log('clickedElement (with plus): ' + clickedElement);
+
+/* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
@@ -26,7 +28,7 @@ for(let activeLink of activeLinks){
 
   /* add class 'active' to the correct article */
 
-
+}
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
